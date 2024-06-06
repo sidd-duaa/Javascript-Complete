946c10e1e662b5cfb7feabd20df972cf77391930
Duaa Siddiqui <123830170+sidd-duaa@users.noreply.github.com>
2024-06-06 14:23:41 +0000

    Variables and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+var accountID = 32;
+let accountEmail = 'hh@gmail.com';
+const accountPassword = '234343';
+accountCity = 'Karachi';
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all code as newer version
+// alert(3 + 3) //nodejs //not in browser
+// Number
+   // int
+   // bigint
+// string
+// boolean
+// null => is itself a value
+// undefined => no value in a declared variable
+// symbol
