4e3853a4cac0f201d5cbc342548a15c9a2f70b61
Duaa Siddiqui <123830170+sidd-duaa@users.noreply.github.com>
2024-08-01 10:25:34 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -78,6 +78,16 @@ console.log("1" + 2 + 2); //122
 console.log(1 + 2 + "2"); //32
 **/
 
+/**
 let gameCounter = 100;
 ++gameCounter;
 console.log(gameCounter);
+**/
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
