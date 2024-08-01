5ffd018adaa5278c58c3f672a1a4d99be91a9248
Duaa Siddiqui <123830170+sidd-duaa@users.noreply.github.com>
2024-08-01 10:02:07 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,11 +43,41 @@ console.log(typeof booleanIsLoggedIn); //boolean
 // "abc" => true 
 // null => false
 
+/**
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+**/ 
 
 // Conversion to String
 // 33 => "33"
+
+// OPERATIONS
+/**
+let value = 3;
+let negValue = -value;
+console.log(negValue); //-3
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+**/
+
+/**
+let str1 = "Hello";
+let str2 = " Duaa";
+let str3 = str1+str2;
+console.log(str3);
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+**/
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
